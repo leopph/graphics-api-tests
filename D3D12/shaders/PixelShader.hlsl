48117adedf08ba03c07097113ae2502b23fca1a9
Levente Löffler <levente.loffler99@gmail.com>
2023-01-11 15:28:18 +0100

    Remove inclusion in shaders from D3D12

--- D3D12/shaders/PixelShader.hlsl
+++ D3D12/shaders/PixelShader.hlsl
@@ -1,1 +1,3 @@
-#include "include/Shader.hlsli"
+float4 PSMain() : SV_TARGET {
+    return float4(1, 1, 1, 1);
+}
--- D3D12/shaders/VertexShader.hlsl
+++ D3D12/shaders/VertexShader.hlsl
@@ -1,1 +1,3 @@
-#include "include/Shader.hlsli"
+float4 VSMain(float2 vertPos : VERTEXPOS) : SV_POSITION {
+    return float4(vertPos, 0, 1);
+}
